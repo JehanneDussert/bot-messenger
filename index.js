c1e2f8ca19178796260f51f5e923db1f55c20863
Jehanne DUSSERT <jdussert@student.42.fr>
2020-10-18 13:43:47 +0200

    hello world

--- index.js
+++ index.js
@@ -3,8 +3,39 @@
 //curl -H "Content-Type: application/json" -X GET "localhost:1337/webhook" -d '{"object": "page", "entry": [{"messaging": [{"message": "TEST_MESSAGE"}]}]}'
 require('dotenv').config();
 
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', function (req, res) {
+	res.send('Hello world, I am a chat bot')
+})
+
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send('Error, wrong token')
+})
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+	console.log('running on port', app.get('port'))
+})
+
 // Imports dependencies and set up http server
-const
+/*const
   express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
@@ -66,4 +97,4 @@ app.get('/webhook', (req, res) => {
 		res.sendStatus(403);      
 	  }
 	}
-  });
+  });*/
