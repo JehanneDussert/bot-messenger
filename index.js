c0943ecfbd61e12ae7e8d572b606f734111eeb18
Jehanne DUSSERT <jdussert@student.42.fr>
2020-10-18 13:25:08 +0200

    bot

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ require('dotenv').config();
 const
   express = require('express'),
   bodyParser = require('body-parser'),
+  start = require('start'),
   app = express().use(bodyParser.json()); // creates express http server
 
 // Sets server port and logs message on success
 //app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 app.set('port', (process.env.PORT || 5000))
+
 // Creates the endpoint for our webhook 
+
 app.post('/webhook', (req, res) => {  
  
 	let body = req.body;
