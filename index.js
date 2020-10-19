b2dcc2277ddf6e514d1157b0636b56c82e210820
Jehanne DUSSERT <jdussert@student.42.fr>
2020-10-19 22:17:49 +0200

    bot

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.post('/webhook/', function (req, res) {
   		    sendGenericMessage(sender)
   		    continue
   	    }
-  	    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+  	    sendTextMessage(sender, "Désolé, je ne comprends pas encore tout le langage humain...")
       }
       if (event.postback) {
   	    let text = JSON.stringify(event.postback)
