75e9250764a79fdf28231c35f0544cdac4807d22
Jehanne DUSSERT <jdussert@student.42.fr>
2020-10-19 21:18:09 +0200

    bot

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
+	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('Error, wrong token')
